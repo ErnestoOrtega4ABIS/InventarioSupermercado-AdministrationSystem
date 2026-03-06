eb2a5bd4866377699dc7bf1dce3b8f7ab46e0714
ErnestoOrtega4ABIS <ernestoortega1423@gmail.com>
2026-03-05 20:39:21 -0600

    doc: Change documentation to English for axios.ts

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -2,12 +2,12 @@
 
 import axios from 'axios';
 
-// Vite expone las variables en import.meta.env
+// Vite expose their environment variables via import.meta.env
 const BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
     baseURL: BASE_URL,
-    withCredentials: true, // Importante para envio/recibir Cookies
+    withCredentials: true, // Very important for cookie-based authentication
     headers: {
         'Content-Type': 'application/json'
     }
